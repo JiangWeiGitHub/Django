6aafa47df80e26aae5b7c2945782c38d3e602038
Jiang Wei <wei.jiang@winsuntech.cn>
2016-02-18 15:41:58 +0800

    update .gitignore.

--- zqxt_views/calc/views.py
+++ zqxt_views/calc/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
  
 def add(request):
     a = request.GET.get('a',0)
@@ -14,3 +17,8 @@ def add(request):
 def add2(request, a, b):
     c = int(a) + int(b)
     return HttpResponse(str(c))
+
+def old_add2_redirect(request, a, b):
+    return HttpResponseRedirect(
+        reverse('add2', args=(a, b))
+    )
--- zqxt_views/zqxt_views/urls.py
+++ zqxt_views/zqxt_views/urls.py
@@ -19,6 +19,7 @@ from calc import views as calc_views
 
 urlpatterns = [
     url(r'^add/', calc_views.add, name='add'),
-    url(r'^add2/(\d+)/(\d+)/$', calc_views.add2, name='add2'),
+    url(r'^add2/(\d+)/(\d+)/$', calc_views.old_add2_redirect),
+    url(r'^new_add/(\d+)/(\d+)/$', 'calc.views.add2', name='add2'),
     url(r'^admin/', admin.site.urls),
 ]
